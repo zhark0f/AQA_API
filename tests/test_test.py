6ec956c21cd0b6e8f108c9409c00d610f00b291c
Yauheni Zharkou <99714788+zhark0f@users.noreply.github.com>
2022-07-08 09:45:33 +0300

    Update test_test.py
    add print in test file

--- tests/test_test.py
+++ tests/test_test.py
@@ -4,3 +4,4 @@ from framework.api.api_requests import MyRequests
 def test_api_requests():
     response = MyRequests.get(url="hello")
     print(response.json())
+    print("Message for test merge conflict")
